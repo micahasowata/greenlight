468f815d6b53944f499158724a8a1c1bccb141a3
Micah Asowata <andrewgary963@gmail.com>
2024-01-19 14:03:34 +0000

    feat: update the writeJSON helper to indent the JSON before response

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -21,7 +21,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 }
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
-	msg, err := json.Marshal(data)
+	msg, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
