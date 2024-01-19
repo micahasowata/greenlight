a1d67b284b5f8ae997464747bc82a47382abd71d
Micah Asowata <andrewgary963@gmail.com>
2024-01-19 18:37:42 +0000

    refactor(routes): update the custom not found handler to `pathNotFoundResponse`

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,7 +9,7 @@ import (
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
-	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.NotFound = http.HandlerFunc(app.pathNotFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
