a7d629b1a74783dd40da3513d89c020c536194f9
Micah Asowata <andrewgary963@gmail.com>
2024-01-19 18:33:25 +0000

    refactor(showMovie): update error handling to use the new function signature

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -15,7 +15,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		app.notFoundResponse(w, r)
+		app.resourceNotFoundResponse(w, r)
 		return
 	}
 
