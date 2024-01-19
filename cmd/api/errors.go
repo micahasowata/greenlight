8ccf055700e8c49d42b2fc27d2fdef109721c30f
Micah Asowata <andrewgary963@gmail.com>
2024-01-19 18:02:24 +0000

    feat(error): add an error response wrapper to handle missing resource response

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -22,3 +22,8 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 	message := "the server encountered a problem and could not process your request"
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
