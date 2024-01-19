c29fe9de42b83befc88741a54f24e9e8c258d770
Micah Asowata <andrewgary963@gmail.com>
2024-01-19 17:54:17 +0000

    feat(error): create a generic error response helper

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,3 +5,13 @@ import "net/http"
 func (app *application) logError(r *http.Request, err error) {
 	app.logger.Print(err)
 }
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	env := envelope{"error": message}
+
+	err := app.writeJSON(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
