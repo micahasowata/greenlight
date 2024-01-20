4a6cce2bce0707c59329ce083dafbfd6d6a12b28
Micah Asowata <andrewgary963@gmail.com>
2024-01-20 20:27:18 +0000

    feat(Movie): a convenience wrapper around the movie data validation rules
    it would make handler code much shorter and readable which improves
    maintainability

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,11 @@
 package data
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/spobly/greenlight/internal/validator"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"`
@@ -10,4 +15,21 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"`
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"`
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be after 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", fmt.Sprintf("must be before %d", int32(time.Now().Year())))
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
