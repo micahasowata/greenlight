4b2cd3ceffdce63594f837fea8b66cee68712518
Micah Asowata <andrewgary963@gmail.com>
2024-01-20 16:32:52 +0000

    feat(readJSON): prevent requests containing multiple data

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -88,5 +88,11 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 			return err
 		}
 	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+
 	return nil
 }
