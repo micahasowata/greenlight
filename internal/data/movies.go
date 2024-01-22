1551be67b80dccd686cf67e9e78753f5a2f07d70
Micah Asowata <andrewgary963@gmail.com>
2024-01-22 08:53:58 +0000

    feat(models): complete the insert method of `MovieModel`

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/lib/pq"
 	"github.com/spobly/greenlight/internal/validator"
 )
 
@@ -40,7 +41,14 @@ type MovieModel struct {
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	query := `
+	INSERT INTO movies (title, year, runtime, genres)
+	VALUES ($1, $2, $3, $4)
+	RETURNING id, created_at, version`
+
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error) {
