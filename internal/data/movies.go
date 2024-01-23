569c8de8ae8886092302b4d745c19ab312ab868b
Micah Asowata <andrewgary963@gmail.com>
2024-01-23 09:33:45 +0000

    refactor(models): enable timeouts with context when retrieveing data

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"fmt"
@@ -58,14 +59,16 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 	}
 
 	query := `
-	SELECT pg_sleep(10), id, created_at, title, year, runtime, genres, version
+	SELECT id, created_at, title, year, runtime, genres, version
 	FROM movies
 	WHERE id = $1`
 
 	var movie Movie
 
-	err := m.DB.QueryRow(query, id).Scan(
-		&[]byte{},
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
 		&movie.ID,
 		&movie.CreatedAt,
 		&movie.Title,
