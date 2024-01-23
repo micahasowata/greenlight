59fb69e4b2b7b8eff17adb6188de345b1bc1f412
Micah Asowata <andrewgary963@gmail.com>
2024-01-23 20:18:21 +0000

    feat(filters): add validation for filter data

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,7 +1,18 @@
 package data
 
+import "github.com/spobly/greenlight/internal/validator"
+
 type Filters struct {
-	Page     int
-	PageSize int
-	Sort     string
+	Page         int
+	PageSize     int
+	Sort         string
+	SortSafeList []string
+}
+
+func (f Filters) Validate(v *validator.Validator) {
+	v.Check(f.Page > 0, "page", "must be greater than zero")
+	v.Check(f.Page <= 10_000_000, "page", "must be less than 10 million")
+	v.Check(f.PageSize > 0, "page_size", "must be greater than zero")
+	v.Check(f.PageSize <= 100, "page_size", "must be less than 100")
+	v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
 }
