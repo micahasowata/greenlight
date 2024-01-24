0f4b2f158790a52b26f8193b49dede1703b15d9f
Micah Asowata <andrewgary963@gmail.com>
2024-01-24 14:03:31 +0000

    feat: remove the `logError` helper and use the `slog` methods

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,22 +5,18 @@ import (
 	"net/http"
 )
 
-func (app *application) logError(r *http.Request, err error) {
-	app.logger.Print(err)
-}
-
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
 	env := envelope{"error": message}
 
 	err := app.writeJSON(w, status, env, nil)
 	if err != nil {
-		app.logError(r, err)
+		app.logger.Error(err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 	}
 }
 
 func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.logError(r, err)
+	app.logger.Error(err.Error())
 
 	message := "the server encountered a problem and could not process your request"
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
