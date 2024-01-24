41c09424fd81386d8cf10b804bcc50f62c3815be
Micah Asowata <andrewgary963@gmail.com>
2024-01-24 15:49:40 +0000

    feat(middleware): add a rate limiting middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -16,4 +18,17 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	limiter := rate.NewLimiter(2, 4)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+
 }
