8245ccb93147d21f98716bdeff46f475698104e4
Micah Asowata <andrewgary963@gmail.com>
2024-01-25 16:00:22 +0000

    feat(User): add validation checks for the data

--- internal/data/users.go
+++ internal/data/users.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"time"
 
+	"github.com/spobly/greenlight/internal/validator"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -17,7 +18,7 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
-	Password  string    `json:"-"`
+	Password  password  `json:"-"`
 	Activated bool      `json:"activated"`
 	Version   int       `json:"-"`
 }
@@ -45,4 +46,30 @@ func (p *password) Get(plaintextPassword string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "password must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func (user *User) Validate(v *validator.Validator) {
+	v.Check(user.Name != "", "name", "name must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	if user.Password.hash != nil {
+		panic("missing password hash for user")
+	}
 }
