2b1808d5adf70ad8059bd0cc5d7365cad05bd9e0
Micah Asowata <andrewgary963@gmail.com>
2024-01-25 15:39:54 +0000

    feat(models): create the `User` type for interacting with user data

--- go.mod
+++ go.mod
@@ -9,3 +9,5 @@ require github.com/lib/pq v1.10.9
 require github.com/joho/godotenv v1.5.1
 
 require golang.org/x/time v0.5.0
+
+require golang.org/x/crypto v0.18.0
---
+++ internal/data/users.go
@@ -0,0 +1,48 @@
+package data
+
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
+	return nil
+}
+
+func (p *password) Get(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
+}
