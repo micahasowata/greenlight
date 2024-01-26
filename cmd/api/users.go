6451948444d13eaaa9300680d227183ba1f25003
Micah Asowata <andrewgary963@gmail.com>
2024-01-26 16:17:00 +0000

    perf(registerUser): process mailing in the background

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -53,13 +53,14 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	err = app.mailer.Send(user, user.Email, "user_welcome.tmpl")
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+	go func() {
+		err = app.mailer.Send(user, user.Email, "user_welcome.tmpl")
+		if err != nil {
+			app.logger.Error(err.Error())
+		}
+	}()
 
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
