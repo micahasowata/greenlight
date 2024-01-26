20a844f356ef191158c6a69fb29aa3b89e329f7f
Micah Asowata <andrewgary963@gmail.com>
2024-01-26 15:15:10 +0000

    feat: expose the mailer to the dependencies

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"flag"
 	"log/slog"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/spobly/greenlight/internal/config"
 	"github.com/spobly/greenlight/internal/data"
+	"github.com/spobly/greenlight/internal/mailer"
 )
 
 const version = "1.0.0"
@@ -16,6 +16,7 @@ type application struct {
 	config config.Config
 	logger *slog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -31,20 +32,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	var cfg config.Config
+	cfg := &config.Config{}
+	cfg.Parse()
 
-	flag.IntVar(&cfg.Port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.Env, "env", "development", "development|staging|production")
-	flag.StringVar(&cfg.Db.DSN, "db-dsn", os.Getenv("DB_DSN"), "PostgresQL dsn")
-	flag.IntVar(&cfg.Db.MaxOpenConns, "db-max-open-conns", 25, "PostgresQL max open connections")
-	flag.IntVar(&cfg.Db.MaxIdleConns, "db-max-idle-conns", 25, "PostgresQL max idle connections")
-	flag.StringVar(&cfg.Db.MaxIdleTime, "db-max-idle-time", "15m", "PostgresQL max connection idle time")
-	flag.Float64Var(&cfg.Limiter.RPS, "limiter-rps", 2, "Rate limiter maximum request per second")
-	flag.IntVar(&cfg.Limiter.Burst, "limiter-burst", 4, "Rate limiter maximum burst")
-	flag.BoolVar(&cfg.Limiter.Enabled, "limiter-enabled", true, "Enable rate limiter")
-	flag.Parse()
-
-	db, err := data.OpenDB(cfg)
+	db, err := data.OpenDB(*cfg)
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
@@ -55,9 +46,10 @@ func main() {
 	logger.Info("database connection pool established")
 
 	app := &application{
-		config: cfg,
+		config: *cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.SMTP.Port, cfg.SMTP.Host, cfg.SMTP.Username, cfg.SMTP.Password, cfg.SMTP.Sender),
 	}
 
 	err = app.serve()
