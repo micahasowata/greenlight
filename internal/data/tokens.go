bb355434a861a56397eef50ee76d1a2866390e00
Micah Asowata <andrewgary963@gmail.com>
2024-01-27 19:12:17 +0000

    feat(tokens): set up the `TokenModel` and its methods

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,8 +1,10 @@
 package data
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/sha256"
+	"database/sql"
 	"encoding/base32"
 	"time"
 
@@ -47,4 +49,56 @@ func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error
 func (t *Token) ValidatePlaintext(v *validator.Validator) {
 	v.Check(t.Plaintext != "", "token", "must be provided")
 	v.Check(len(t.Plaintext) == 26, "token", "must be 26 bytes long")
+}
+
+type TokenModel struct {
+	DB *sql.DB
+}
+
+func (m TokenModel) Insert(token *Token) error {
+	query := `
+	INSERT INTO tokens (hash, user_id, expiry, scope)
+	VALUES ($1, $2, $3, $4)`
+
+	args := []any{token.Hash, token.UserID, token.Expiry, token.Scope}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, args)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token, err := generateToken(userID, ttl, scope)
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.Insert(token)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func (m TokenModel) DeleteAllForUser(scope string, userID int64) error {
+	query := `
+	DELETE FROM users
+	WHERE scope = $1 AND user_id = $2`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, scope, userID)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
