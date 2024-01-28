425b341bb3b772447c7d3a012b97db0888fc5ef3
Micah Asowata <andrewgary963@gmail.com>
2024-01-28 17:02:09 +0000

    refactor(registerUser): generate token and add send its plaintext with the email

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/spobly/greenlight/internal/data"
 	"github.com/spobly/greenlight/internal/validator"
@@ -53,8 +54,19 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user, user.Email, "user_welcome.tmpl")
+		data := map[string]string{
+			"activationToken": token.Plaintext,
+			"name":            user.Name,
+		}
+
+		err = app.mailer.Send(data, user.Email, "user_welcome.tmpl")
 		if err != nil {
 			app.logger.Error(err.Error())
 		}
