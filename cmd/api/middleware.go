90ec6d37a3933dcdf4555a57c8b9f740e6fb4997
Micah Asowata <andrewgary963@gmail.com>
2024-01-29 18:43:12 +0000

    feat(middleware): set up a `requireAuthenticatedUser` middleware

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -127,6 +127,19 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 	})
 }
 
+func (app *application) requireAuthenticatedUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		if user.IsAnonymous() {
+			app.authenticationRequiredResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user := app.contextGetUser(r)
