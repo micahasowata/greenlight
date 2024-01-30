01b99c6c63aa2d59a79a1e205af0ffd99fa24551
Micah Asowata <andrewgary963@gmail.com>
2024-01-30 15:36:56 +0000

    refactor(cors): enable the middleware to catch preflight cors and respond to it

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -180,6 +180,8 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Origin")
 
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
 		origin := r.Header.Get("Origin")
 
 		if origin != "" {
@@ -187,6 +189,15 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 			for _, o := range app.config.CORS.TrustedOrigins {
 				if o == origin {
 					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+
 					break
 				}
 			}
