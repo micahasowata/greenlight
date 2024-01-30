b798486bde98ba1ea2a2a8cf7cf26294632c2305
Micah Asowata <andrewgary963@gmail.com>
2024-01-30 10:21:38 +0000

    feat(permissions): add method on `PermissionsModel` to grant user permissions

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -59,3 +59,19 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 
 	return permissions, nil
 }
+
+func (m PermissionModel) AddForUser(userID int64, codes ...string) error {
+	query := `
+	INSERT INTO users_permissions
+	SELECT $1, permissions.id FROM permissions WHERE permissions.code = ANY($2)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, userID, codes)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
