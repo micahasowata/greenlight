6f5f4cfdf55cdc28565a96135db88495de6d6d11
Micah Asowata <andrewgary963@gmail.com>
2024-01-30 22:47:17 +0000

    feat(metrics): add more application specific metrics to the watch-list

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,9 @@ import (
 	"expvar"
 	"log/slog"
 	"os"
+	"runtime"
 	"sync"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/spobly/greenlight/internal/config"
@@ -48,6 +50,18 @@ func main() {
 	logger.Info("database connection pool established")
 
 	expvar.NewString("version").Set(version)
+
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+
+	expvar.Publish("timestamp", expvar.Func(func() any {
+		return time.Now().Unix()
+	}))
 
 	app := &application{
 		config: *cfg,
