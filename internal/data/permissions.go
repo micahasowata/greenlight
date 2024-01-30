c107d7155a3d98456d7a7d0d65fb18569679d48e
Micah Asowata <andrewgary963@gmail.com>
2024-01-30 09:59:56 +0000

    refactor(permissions): remove duplicate calls to the `users_permissions` table

--- internal/data/permissions.go
+++ internal/data/permissions.go
@@ -26,8 +26,8 @@ func (m PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
 	query := `
 	SELECT permissions.code
 	FROM permissions
-	INNER JOIN users_permissions ON users_permissions.permissions_id = permissions.id
-	INNER JOIN users_permissions ON users_permissions.user_id = users.id
+	INNER JOIN users_permissions ON users_permissions.permission_id = permissions.id
+	INNER JOIN users ON users_permissions.user_id = users.id
 	WHERE users.id = $1`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
