2303554a30e89dcae7e13081ccb6300b646ca56f
Micah Asowata <andrewgary963@gmail.com>
2024-01-31 12:09:57 +0000

    refactor(middleware): extract the client exact IP address from the request

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"expvar"
 	"fmt"
-	"net"
 	"net/http"
 	"strings"
 	"sync"
@@ -13,6 +12,7 @@ import (
 	"github.com/spobly/greenlight/internal/data"
 	"github.com/spobly/greenlight/internal/metrics"
 	"github.com/spobly/greenlight/internal/validator"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -56,11 +56,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if app.config.Limiter.Enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
